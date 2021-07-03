9876938e0e0b1128b568e3bb74d1daefd98cce26
briansosa <sosa_brian@live.com>
2021-07-03 15:20:22 -0300

    add Facade

---
+++ src/Facade/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Facade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Subsystem1 subsystem1 = new Subsystem1();
+            Subsystem2 subsystem2 = new Subsystem2();
+            Facade facade = new Facade(subsystem1, subsystem2);
+            Client.ClientCode(facade);
+        }
+    }
+    class Client
+    {
+        public static void ClientCode(Facade facade)
+        {
+            Console.Write(facade.Operation());
+        }
+    }
+    public class Facade
+    {
+        protected Subsystem1 subsystem1;
+        
+        protected Subsystem2 subsystem2;
+
+        public Facade(Subsystem1 subsystem1, Subsystem2 subsystem2)
+        {
+            this.subsystem1 = subsystem1;
+            this.subsystem2 = subsystem2;
+        }
+        
+        public string Operation()
+        {
+            string result = "Facade initializes subsystems:\n";
+            result += this.subsystem1.operation1();
+            result += this.subsystem2.operation1();
+            result += "Facade orders subsystems to perform the action:\n";
+            result += this.subsystem1.operationN();
+            result += this.subsystem2.operationZ();
+            return result;
+        }
+    }
+    public class Subsystem1
+    {
+        public string operation1()
+        {
+            return "Subsystem1: Ready!\n";
+        }
+
+        public string operationN()
+        {
+            return "Subsystem1: Go!\n";
+        }
+    }
+    
+    // Some facades can work with multiple subsystems at the same time.
+    public class Subsystem2
+    {
+        public string operation1()
+        {
+            return "Subsystem2: Get ready!\n";
+        }
+
+        public string operationZ()
+        {
+            return "Subsystem2: Fire!\n";
+        }
+    }
+}
